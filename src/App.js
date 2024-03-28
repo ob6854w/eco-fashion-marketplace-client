153ec56c916cbd1b2c3252136ab64e1dd4a647e9
Oluwayemisi Bakare <tosine727@gmail.com>
2024-03-28 12:41:04 -0400

    completed section component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero"
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.scss";
+import Section from "./components/Section/Section";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero/>
+      <Section/>
     </div>
   );
 }
---
+++ src/components/Section/Section.js
@@ -0,0 +1,16 @@
+import "./Section.scss"
+
+function Section() {
+  return (
+    <div className="section">
+      <div className="section__wrapper">
+        <p>
+          Elevate your lifestyle with a more intelligent, superior wardrobe. Our
+          range is crafted sustainably with longevity in mind.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Section;
