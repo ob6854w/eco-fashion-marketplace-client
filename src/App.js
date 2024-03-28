dee64f127f67075c6567e255b43c97c1364ec1f7
Oluwayemisi Bakare <tosine727@gmail.com>
2024-03-28 11:30:35 -0400

    completed the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Navbar from "./components/Navbar/Navbar";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.scss";
+
 function App() {
   return (
     <div className="App">
-     
-  
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,32 @@
-import logo from "../../assets/images/E-logo.jpg";
-import { Link } from "react-router-dom";
+import logo from "../../assets/images/Logo.png";
+import { CiShoppingCart } from "react-icons/ci";
+import { HiBars3 } from "react-icons/hi2";
+import { IoIosSearch } from "react-icons/io";
+import { FaPortrait } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+
 import "./Navbar.scss";
-function Navbar(){
-    return (
-        <nav className="navbar">
-        <div className="navbar__container"> 
-        <Link to="/">
-        <img className="navbar__logo" src={logo} alt="logo"/>
-        </Link>
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__container">
+        <HiBars3 className="navbar__container-icon" />
+        <div className="navbar__container-wrapper">
+          <img className="navbar__logo" src={logo} alt="logo" />
+          <div className="navbar__container-menu">
+            <a>Home</a>
+            <a>About Us</a>
+            <a>Contact Us</a>
+          </div>
         </div>
-        </nav>
-    )
+        <div className="navbar__icons-container">
+          <IoIosSearch className="navbar__icons-container-icon" />
+          <FaPortrait className="navbar__icons-container-icon" />
+          <CiShoppingCart className="navbar__icons-container-icon" />
+        </div>
+      </div>
+    </nav>
+  );
 }
-
-
-
 
 export default Navbar;
