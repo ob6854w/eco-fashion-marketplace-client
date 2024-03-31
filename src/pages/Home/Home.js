8d4ef1c24652330a5e261fe74bb9651be3aed3b5
Oluwayemisi Bakare <tosine727@gmail.com>
2024-03-31 18:58:34 -0400

    still working on the pages

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,34 +12,7 @@ import Footer from "../../components/Footer/Footer";
 
 
 function Home() {
-    // const [firstVideo, setFirstVideo] = useState(null);
-  
-    // useEffect(() => {
-    //   const getFirstVideo = async () => {
-    //     try {
-    //       //get all videos
-    //       const response = await axios.get(`${REACT_APP_BASE_URL}/videos`);
-  
-    //       //get first video only
-    //       const firstVideo = response.data.shift();
-  
-    //       const result = await axios.get(
-    //         `${REACT_APP_BASE_URL}/videos/${firstVideo.id}`
-    //       );
-  
-    //       setFirstVideo(result.data);
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   };
-  
-    //   getFirstVideo();
-    // }, []);
-  
-    // if (!firstVideo) {
-    //   return <div>Loading...</div>;
-    // }
-  
+    
     return (
       <>
       <Hero />
